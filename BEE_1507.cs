214f6cc1f02a5bd8e369f77f85c9d3fcf7535956
Dhiego Martins Andrade <mdhiego@gmail.com>
2022-05-12 09:42:43 -0300

    Solve problem 1507 at Beecrowd

---
+++ BEE_1507.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+
+        var testCases = new Dictionary<string, IEnumerable<string>>();
+
+        for (int i = 0; i < N; i++)
+        {
+            string testCase = Console.ReadLine();
+
+            int Q = int.Parse(Console.ReadLine());
+
+            string[] queries = new string[Q];
+            for (int j = 0; j < Q; j++)
+                queries[j] = Console.ReadLine();
+
+            testCases.Add(testCase, queries);
+        }
+
+        foreach (var testCase in testCases)
+            foreach (var query in testCase.Value)
+                Console.WriteLine(IsSubString(testCase.Key, query) ? "Yes" : "No");
+    }
+
+    private static bool IsSubString(string word, string query)
+    {
+        int j = 0;
+        for (int i = 0; i < word.Length && j < query.Length; i++)
+            if (query[j] == word[i])
+                j++;
+
+        return j == query.Length;
+    }
+}

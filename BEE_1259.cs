eea56de0b6428ee8b315b6719e286158a519c5db
Dhiego Martins Andrade <mdhiego@gmail.com>
2022-05-12 09:42:13 -0300

    Solve problem 1259 at Beecrowd

---
+++ BEE_1259.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Linq;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+
+        int[] numbers = new int[N];
+
+        for (int i = 0; i < N; i++)
+            numbers[i] = int.Parse(Console.ReadLine());
+
+        int[] evenNumbers = numbers.Where(x => x % 2 == 0).ToArray();
+        int[] oddNumbers = numbers.Where(x => x % 2 != 0).ToArray();
+
+        foreach (var evenNumber in evenNumbers.OrderBy(x => x))
+            Console.WriteLine(evenNumber);
+
+        foreach (var oddNumber in oddNumbers.OrderByDescending(x => x))
+            Console.WriteLine(oddNumber);
+    }
+}

06c768af15d6569a949239babfadbf02ecc5f3db
Dhiego Martins Andrade <mdhiego@gmail.com>
2022-05-12 09:39:16 -0300

    Solve problem 1010 at Beecrowd

---
+++ BEE_1010.cs
@@ -0,0 +1,16 @@
+using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        var input1 = Console.ReadLine().Split(' ');
+        var product1 = (Code: int.Parse(input1[0]), NumberOfUnits: int.Parse(input1[1]), Price: double.Parse(input1[2]));
+        var input2 = Console.ReadLine().Split(' ');
+        var product2 = (Code: int.Parse(input2[0]), NumberOfUnits: int.Parse(input2[1]), Price: double.Parse(input2[2]));
+
+        double totalPrice = product1.NumberOfUnits * product1.Price + product2.NumberOfUnits * product2.Price;
+
+        Console.WriteLine($"VALOR A PAGAR: R$ {totalPrice:F2}");
+    }
+}

bb8a3ff10881b198d5df24f06a697c3319951d30
Dhiego Martins Andrade <mdhiego@gmail.com>
2022-05-12 09:40:39 -0300

    Solve problem 1020 at Beecrowd

---
+++ BEE_1020.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        int daysOfLife = int.Parse(Console.ReadLine());
+
+        int rest = daysOfLife;
+
+        var timeMeasures = new Dictionary<int, string>()
+        {
+            {365, "ano(s)"},
+            {30, "mes(es)"},
+            {1, "dia(s)"}
+        };
+
+        foreach (var timeMeasure in timeMeasures)
+        {
+            Console.WriteLine($"{rest / timeMeasure.Key} {timeMeasure.Value}");
+            rest %= timeMeasure.Key;
+        }
+    }
+}
